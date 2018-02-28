ed9e946ece40b2579071daaa8d2df7e031ec97bf
Joy Dragon <joydragon@gmail.com>
2018-02-28 17:38:38 -0300

    Create upload_sample.sh

---
+++ upload_sample.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function usage(){
+        echo ""
+        echo "    $0 <event_id> <attachment file>"
+        exit 1
+}
+
+if [ $# -ne 2 ]; then
+        echo "Error: need two parameters."
+        usage
+fi
+
+re_int='^[0-9]+$'
+event_id="$1"
+if [[ ! $event_id =~ $re_int ]]; then
+        echo "Error:, event_id is not an integer."
+        usage
+fi
+
+FILE="$2"
+if [ ! -f "$FILE" ]; then
+        echo "Error: file does not exist."
+        usage
+fi
+
+DATA=$(base64 -w0 "$FILE")
+
+BASE_URL="https://misp.example.com/"
+AUTHORIZATION_HEADER="Authorization: [auth key]"
+
+ATTACHMENTS='{"request":{"files": [{"filename": "'$1'", "data": "'$DATA'"}]}}'
+
+echo -e "$ATTACHMENTS"
+exit
+
+tempfile=$(mktemp)
+echo -e "$ATTACHMENTS" > "$tempfile"
+CURL=$(curl -s -k "${BASE_URL}events/upload_sample/$event_id" -H "$AUTHORIZATION_HEADER" -H "Accept: application/json" -H "Content-Type: application/json" --data "@$tempfile" -XPOST)
+rm -f $tempfile
+
+echo -e "$CURL"
